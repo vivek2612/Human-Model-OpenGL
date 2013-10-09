aaec23405cab9c3667b7c3212809662a47689a51
Ved Ratn Dixit <ved050493@gmail.com>
2013-10-10 01:49:39 +0530

    Most of the key assignment and movement parameter boundaries are set. Slight tweaking may be required to make it better, but its still presentable sort of :p

--- src/human.cpp
+++ src/human.cpp
@@ -136,15 +136,17 @@ void Human::drawHuman(){
 		
 		// hip
 		glTranslatef(hipTranslatex,hipTranslatey,hipTranslatez);
+		glTranslatef(0.0f, -1.0f, 0.0f);
 		glRotatef(hipx,1.0,0.0,0.0);
-		glRotatef(hipy,0.0,1.0,0.0);
 		glRotatef(hipz,0.0,0.0,1.0);
+		glRotatef(hipy,0.0,1.0,0.0);
+		glTranslatef(0.0f, 1.0f, 0.0f);
 		glCallList(hipList);
 		glPushMatrix();
 			glTranslatef(-1.1f,-1.0f,0.0f);
 			glRotatef(leftUpperLegx,1.0,0.0,0.0);
-			glRotatef(leftUpperLegy,0.0,1.0,0.0);
 			glRotatef(leftUpperLegz,0.0,0.0,1.0);
+			glRotatef(leftUpperLegy,0.0,1.0,0.0);
 			glCallList(upperLegList);															
 			glTranslatef(0.0f,-4.0f,0.0f);
 			glRotatef(leftLowerLegx,1.0,0.0,0.0);
@@ -154,25 +156,25 @@ void Human::drawHuman(){
 		glPushMatrix();
 			glTranslatef(1.1f,-1.0f,0.0f);
 			glRotatef(rightUpperLegx,1.0,0.0,0.0);
-			glRotatef(rightUpperLegy,0.0,1.0,0.0);
 			glRotatef(rightUpperLegz,0.0,0.0,1.0);
+			glRotatef(rightUpperLegy,0.0,1.0,0.0);
 			glCallList(upperLegList);
 			glTranslatef(0.0f,-4.0f,0.0f);
 			glRotatef(rightLowerLegx,1.0,0.0,0.0);
 			glCallList(lowerLegList);
 		glPopMatrix();
 
-		glTranslatef(0.0f, 5.0f, 0.0f);
 		glRotatef(torsox,1.0,0.0,0.0);
-		glRotatef(torsoy,0.0,1.0,0.0);
 		glRotatef(torsoz,0.0,0.0,1.0);
+		glRotatef(torsoy,0.0,1.0,0.0);
+		glTranslatef(0.0f, 5.0f, 0.0f);
 		glCallList(torsoList);
 
 		glPushMatrix();
 			glTranslatef(2.5f,-0.0f,0.0f);
 			glRotatef(rightUpperArmx,1.0,0.0,0.0);
-			glRotatef(rightUpperArmy,0.0,1.0,0.0);
 			glRotatef(rightUpperArmz,0.0,0.0,1.0);
+			glRotatef(rightUpperArmy,0.0,1.0,0.0);
 			glCallList(upperArmList);
 
 			glTranslatef(0.0,-3.0f,0.0f);
@@ -183,8 +185,8 @@ void Human::drawHuman(){
 		glPushMatrix();
 			glTranslatef(-2.5f,-0.0f,0.0f);
 			glRotatef(leftUpperArmx,1.0,0.0,0.0);
-			glRotatef(leftUpperArmy,0.0,1.0,0.0);
 			glRotatef(leftUpperArmz,0.0,0.0,1.0);
+			glRotatef(leftUpperArmy,0.0,1.0,0.0);
 			glCallList(upperArmList);
 
 			glTranslatef(0.0,-3.0f,0.0f);
@@ -192,17 +194,17 @@ void Human::drawHuman(){
 			glCallList(lowerArmList);
 		glPopMatrix();
 
-		glTranslatef(0.0f,0.6f,0.0f);
+		
 		glRotatef(neckx,1.0,0.0,0.0);
-		glRotatef(necky,0.0,1.0,0.0);
 		glRotatef(neckz,0.0,0.0,1.0);
-
+		glRotatef(necky,0.0,1.0,0.0);
+		glTranslatef(0.0f,0.6f,0.0f);
 		glCallList(neckList);
 
-		glTranslatef(0.0f,1.3f,0.0f);
 		glRotatef(headx,1.0,0.0,0.0);
-		glRotatef(heady,0.0,1.0,0.0);
 		glRotatef(headz,0.0,0.0,1.0);
+		glRotatef(heady,0.0,1.0,0.0);
+		glTranslatef(0.0f,1.3f,0.0f);
 		glCallList(headList);
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,20 @@
 #include <cstdio>
 #include "box.h"
 #include "human.h"
+
+#define _USE_MATH_DEFINES
+
 /* Global variables */
 char title[] = "3D Shapes";
 float degree = 0.0f;
 float verticalDegree = 0.0f;
 Box boxObject;
+
+
+
 float eyex=0.0f,eyey=0.0f,eyez=0.0f;
 Human humanObject;
-
+int mode = -1;
 /* Initialize OpenGL Graphics */
 void initGL() {
    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
@@ -35,7 +41,7 @@ void display() {
    glMatrixMode(GL_MODELVIEW);     // To operate on model-view matrix
    gluLookAt(eyex, eyey, eyez, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
    glLoadIdentity();
-   glTranslatef( 0.0f, 0.0f, -30.0f);
+   glTranslatef( 0.0f, 0.0f, -20.0f);
    glRotatef(degree, 0.0f, 1.0f, 0.0f);
    glRotatef(verticalDegree, 1.0f, 0.0f, 0.0f);
    
@@ -76,12 +82,105 @@ void inputKey(int key, int x, int y){
    }
 }
 
+/*Body movement scheme is designed in the following way
+There are modes which designate which body part's degree of freedom are controlled by the keys 'z', 'x' and 'c'
+Mode Index :
+	0: Hip-Translation
+	1: Head
+	2: Neck
+	3: Torso
+	4: Left-Upper-Arm
+	5: Left-Lower-Arm
+	6: Right-Upper-Arm
+	7: Right-Lower-Arm
+	8: Hip
+	'p': Left-Upper-Leg
+	'o': Left-Lower-Leg
+	'i': Right-Upper-Leg
+	'u': Right-Lower-Leg
+
+If key 'z' would would increase a certain parameter of movement of some joint 'Shift'+'z' or 'Z' would decrease it
+
+Other than that the box lid angle is controlled by '=' and '-'
+
+All joints and box-lid have certain boundaries till which they can rotate in various directions
+
+Also w, s, a & d control the camera
+*/
+
+
+
 void keyboard(unsigned char key, int x, int y){
    switch(key){
       case 27:
       {
          exit(0);
          break;
+      }
+      case '0':{
+  		mode = 0;
+	    glutPostRedisplay();
+	    break;
+      }
+      case '1':{
+  		mode = 1;
+	    glutPostRedisplay();
+	    break;
+      }
+      case '2':{
+  		mode = 2;
+	    glutPostRedisplay();
+	    break;
+      }
+      case '3':{
+  		mode = 3;
+	    glutPostRedisplay();
+	    break;
+      }
+      case '4':{
+  		mode = 4;
+	    glutPostRedisplay();
+	    break;
+      }
+      case '5':{
+  		mode = 5;
+	    glutPostRedisplay();
+	    break;
+      }
+      case '6':{
+  		mode = 6;
+	    glutPostRedisplay();
+	    break;
+      }
+      case '7':{
+  		mode = 7;
+	    glutPostRedisplay();
+	    break;
+      }
+      case '8':{
+  		mode = 8;
+	    glutPostRedisplay();
+	    break;
+      }
+      case 'p':{
+  		mode = 9;
+	    glutPostRedisplay();
+	    break;
+      }
+      case 'o':{
+  		mode = 10;
+	    glutPostRedisplay();
+	    break;
+      }
+      case 'i':{
+  		mode = 11;
+	    glutPostRedisplay();
+	    break;
+      }
+      case 'u':{
+  		mode = 12;
+	    glutPostRedisplay();
+	    break;
       }
       case '=':
       {
@@ -121,25 +220,395 @@ void keyboard(unsigned char key, int x, int y){
          glutPostRedisplay();
          break;
       }
-      case 'l':
-      {  
-         humanObject.leftUpperArmx+=2.0f;
-         glutPostRedisplay();
-         break;
+      case 'z':{
+  		if(mode>=0){
+  			if(mode == 0){
+  				//Hip Translation
+  				humanObject.hipTranslatez += 0.1f;
+  			}
+  			else if (mode == 1){
+  				// Head Rotation
+  				if(humanObject.headz <= 45.0f)
+  					humanObject.headz += 2.0f;
+  			}
+  			else if (mode == 2){
+  				// Neck Rotation
+  				if(humanObject.neckz <= 25.0f)
+  					humanObject.neckz += 2.0f;
+  			}
+  			else if (mode == 3){
+  				// Torso Rotation
+  				if(humanObject.torsoz <= 25.0f)
+  					humanObject.torsoz += 2.0f;
+  			}
+  			else if (mode == 4){
+  				if(humanObject.leftUpperArmz<=0.0f)
+  					humanObject.leftUpperArmz+=2.0f;
+  			}
+  			else if (mode == 6){
+  				if(humanObject.rightUpperArmz<180.0f)
+  					humanObject.rightUpperArmz+=2.0f;
+  			}
+  			else if(mode == 8){
+  				if(humanObject.hipz < 45.0f)
+  					humanObject.hipz += 2.0f;
+  			}
+  			else if(mode == 9){
+  				if(humanObject.leftUpperLegz < 0.0f)
+  					humanObject.leftUpperLegz += 2.0f;
+  			}
+  			else if(mode == 11){
+  				if(humanObject.rightUpperLegz < 45.0f)
+  					humanObject.rightUpperLegz += 2.0f;
+  			}
+  		}
+	    glutPostRedisplay();
+	    break;
       }
-      case 'm':
-      {  
-         humanObject.leftLowerArmx-=2.0f;
-         glutPostRedisplay();
-         break;
+      case 'Z':{
+  		if(mode>=0){
+  			if(mode == 0){
+  				//Hip Translation
+  				humanObject.hipTranslatez -= 0.1f;
+  			}
+  			else if (mode == 1){
+  				// Head Rotation
+  				if(humanObject.headz >= -45.0f)
+  					humanObject.headz -= 2.0f;
+  			}
+  			else if (mode == 2){
+  				// Neck Rotation
+  				if(humanObject.neckz >= -25.0f)
+  					humanObject.neckz -= 2.0f;
+  			}
+  			else if (mode == 3){
+  				// Neck Rotation
+  				if(humanObject.torsoz >= -25.0f)
+  					humanObject.torsoz -= 2.0f;
+  			}
+  			else if(mode == 4){
+  				// cout<<"AAYA"<<endl;
+  				if(humanObject.leftUpperArmz >= -180.0f)
+  					humanObject.leftUpperArmz -= 2.0f;
+  			}
+  			else if(mode == 6){
+  				// cout<<"AAYA"<<endl;
+  				if(humanObject.rightUpperArmz > 0)
+  					humanObject.rightUpperArmz -= 2.0f;
+  			}
+  			else if(mode == 8){
+  				if(humanObject.hipz > -45.0f)
+  					humanObject.hipz -= 2.0f;
+  			}
+  			else if(mode == 9){
+  				if(humanObject.leftUpperLegz > -45.0f)
+  					humanObject.leftUpperLegz -= 2.0f;
+  			}
+  			else if(mode == 11){
+  				if(humanObject.rightUpperLegz > 0.0f)
+  					humanObject.rightUpperLegz -= 2.0f;
+  			}
+  		}
+	    glutPostRedisplay();
+	    break;
       }
-      case 'n':
-      {  
-         humanObject.leftUpperArmx-=2.0f;
-         glutPostRedisplay();
-         break;
+      case 'x':{
+      	if(mode >= 0){
+      		if(mode == 0){
+      			//Hip Translation
+      			humanObject.hipTranslatex += 0.1f;
+      		}
+      		else if(mode == 1){
+      			// Head Rotation
+      			if(humanObject.headx<=45.0f)
+      				humanObject.headx+=2.0f;
+      		}
+      		else if(mode == 2){
+      			// Neck Rotation
+      			if(humanObject.neckx<=25.0f)
+      				humanObject.neckx+=2.0f;
+      		}
+      		else if(mode == 3){
+      			// Neck Rotation
+      			if(humanObject.torsox<=25.0f)
+      				humanObject.torsox+=2.0f;
+      		}
+      		else if(mode == 4){
+      			// Upper Arm Rotation
+      			humanObject.leftUpperArmx+=2.0f;
+      		}
+      		else if(mode == 5){
+      			if(humanObject.leftLowerArmx <= 0.0f)
+      				humanObject.leftLowerArmx +=2.0f;
+      		}
+      		else if(mode == 6){
+      			// Upper Arm Rotation
+      			humanObject.rightUpperArmx+=2.0f;
+      		}
+      		else if(mode == 7){
+      			if(humanObject.rightLowerArmx <= 0.0f)
+      				humanObject.rightLowerArmx +=2.0f;
+      		}
+      		else if(mode == 8){
+      			if(humanObject.hipx < 45.0f)
+      				humanObject.hipx += 2.0f;
+      		}
+      		else if(mode == 9){
+      			if(humanObject.leftUpperLegx < 60.0f)
+      				humanObject.leftUpperLegx += 2.0f;
+      		}
+      		else if(mode == 10){
+      			if(humanObject.leftLowerLegx < 120.0f)
+      				humanObject.leftLowerLegx += 2.0f;
+      		}
+      		else if(mode == 11){
+      			if(humanObject.rightUpperLegx < 60.0f)
+      				humanObject.rightUpperLegx += 2.0f;
+      		}
+      		else if(mode == 12){
+      			if(humanObject.rightLowerLegx < 120.0f)
+      				humanObject.rightLowerLegx += 2.0f;
+      		}
+      	}
+      	glutPostRedisplay();
+	    break;
       }
+      case 'X':{
+      	if(mode >= 0){
+      		if(mode == 0){
+      			//Hip Translation
+      			humanObject.hipTranslatex -= 0.1f;
+      		}
+      		else if(mode == 1){
+      			// Head Rotation
+      			if(humanObject.headx>=-45.0f)
+      				humanObject.headx-=2.0f;
+      		}
+      		else if(mode == 2){
+      			// Head Rotation
+      			if(humanObject.neckx>=-25.0f)
+      				humanObject.neckx-=2.0f;
+      		}
+      		else if(mode == 3){
+      			// Head Rotation
+      			if(humanObject.torsox>=-25.0f)
+      				humanObject.torsox-=2.0f;
+      		}
+      		else if(mode == 4){
+      			//Upper Arm Rotation
+      			humanObject.leftUpperArmx-=2.0f;
+      		}
+      		else if(mode == 5){
+      			if(((int)humanObject.leftUpperArmx )%360<= 10.0f && ((int)humanObject.leftUpperArmx)%360 >= -10.0f && humanObject.leftUpperArmy >= 45.0f){
+      				cout<<"First"<<":"<<humanObject.leftUpperArmx<<":"<<humanObject.leftUpperArmy<<endl;
+      				if(humanObject.leftLowerArmx >= -1*(180 - ((90+humanObject.leftUpperArmz)+(180.0/M_PI)*acos(0.75*(sin(-1*humanObject.leftUpperArmz))))))
+      					humanObject.leftLowerArmx -= 2.0f;
+      			}
+      			else{
+      				cout<<"second"<<":"<<humanObject.leftUpperArmx<<":"<<humanObject.leftUpperArmy<<endl;
+      				if(humanObject.leftLowerArmx >= -135.0f )
+      					humanObject.leftLowerArmx -= 2.0f;
+      			}
+      		}
+      		else if(mode == 6){
+      			//Upper Arm Rotation
+      			humanObject.rightUpperArmx-=2.0f;
+      		}
+      		else if(mode == 7){
+      			if(((int)humanObject.rightUpperArmx )%360<= 10.0f && ((int)humanObject.rightUpperArmx)%360 >= -10.0f && humanObject.rightUpperArmy <= -45.0f){
+      				// cout<<"First"<<":"<<humanObject.leftUpperArmx<<":"<<humanObject.leftUpperArmy<<endl;
+      				if(humanObject.rightLowerArmx >= -1*(180 - ((90-humanObject.rightUpperArmz)+(180.0/M_PI)*acos(0.75*(sin(humanObject.rightUpperArmz))))))
+      					humanObject.rightLowerArmx -= 2.0f;
+      			}
+      			else{
+      				// cout<<"second"<<":"<<humanObject.leftUpperArmx<<":"<<humanObject.leftUpperArmy<<endl;
+      				if(humanObject.rightLowerArmx >= -135.0f )
+      					humanObject.rightLowerArmx -= 2.0f;
+      			}
+      		}
+      		else if(mode == 8){
+      			if(humanObject.hipx > -45.0f)
+      				humanObject.hipx -= 2.0f;      		
+      		}
+      		else if(mode == 9){
+      			if(humanObject.leftUpperLegx > -90.0f)
+      				humanObject.leftUpperLegx -= 2.0f;
+      		}
+      		else if(mode == 10){
+      			if(humanObject.leftLowerLegx > 0.0f)
+      				humanObject.leftLowerLegx -= 2.0f;
+      		}
+      		else if(mode == 11){
+      			if(humanObject.rightUpperLegx > -90.0f)
+      				humanObject.rightUpperLegx -= 2.0f;
+      		}
+      		else if(mode == 12){
+      			if(humanObject.rightLowerLegx > 0.0f)
+      				humanObject.rightLowerLegx -= 2.0f;
+      		}
+      	}
+      	glutPostRedisplay();
+	    break;
+      }
+      case 'c':{
+      	if(mode >= 0){
+      		if(mode == 0){
+      			//Hip Translation
+      			humanObject.hipTranslatey += 0.1f;
+      		}
+      		else if(mode == 1){
+      			// Head Rotation
+      			if(humanObject.heady<=60.0f)
+      				humanObject.heady+=2.0f;
+      		}
+      		else if(mode == 2){
+      			// Head Rotation
+      			if(humanObject.necky<=45.0f)
+      				humanObject.necky+=2.0f;
+      		}
+      		else if(mode == 3){
+      			// Head Rotation
+      			if(humanObject.torsoy<=45.0f)
+      				humanObject.torsoy+=2.0f;
+      		}
+      		else if(mode == 4){
+      			// Left Upper Arm Rotation
+      			// cout<<(90+humanObject.leftUpperArmz)<<":"<<(180.0/M_PI)*acos(0.75*(sin(-1*humanObject.leftUpperArmz))) <<endl;
+      			if(humanObject.leftUpperArmz < -90.0f){
+      				if(humanObject.leftUpperArmy <= 90.0f){
+      					humanObject.leftUpperArmy += 2.0f;
+      				}
+      			}
+      			else if(humanObject.leftLowerArmx > (180 - ((90+humanObject.leftUpperArmz) + (180.0/M_PI)*acos(0.75*(sin(-1*humanObject.leftUpperArmz))) ))){
+      				if(humanObject.leftUpperArmy <= 30.0f)
+      					humanObject.leftUpperArmy += 2.0f;
+      			}
+      			else
+      				if(humanObject.leftUpperArmy <= 90.0f){
+      					humanObject.leftUpperArmy += 2.0f;
+      				}
+      		}
+      		else if(mode == 6){
+      			// Head Rotation
+      			if(humanObject.rightUpperArmy<90.0f)
+      				humanObject.rightUpperArmy+=2.0f;
+      		}
+      		else if(mode == 8){
+      			if(humanObject.hipy < 30.0f)
+      				humanObject.hipy += 2.0f;
+      		}
+      		else if(mode == 9){
+      			if(humanObject.leftUpperLegy < 30.0f)
+      				humanObject.leftUpperLegy += 2.0f;
+      		}
+      		else if(mode == 11){
+      			if(humanObject.rightUpperLegy < 30.0f)
+      				humanObject.rightUpperLegy += 2.0f;
+      		}
+      	}
+      	glutPostRedisplay();
+	    break;	
+      }
+      case 'C':{
+      	if(mode >= 0){
+      		if(mode == 0){
+      			//Hip Translation
+      			humanObject.hipTranslatey -= 0.1f;
+      		}
+      		else if(mode == 1){
+      			// Head Rotation
+      			if(humanObject.heady>=-60.0f)
+      				humanObject.heady-=2.0f;
+      		}
+      		else if(mode == 2){
+      			// Head Rotation
+      			if(humanObject.necky>=-45.0f)
+      				humanObject.necky-=2.0f;
+      		}
+      		else if(mode == 3){
+      			// Head Rotation
+      			if(humanObject.torsoy>=-45.0f)
+      				humanObject.torsoy-=2.0f;
+      		}
+      		else if(mode == 4){
+      			// Head Rotation
+      			if(humanObject.leftUpperArmy>=-90.0f)
+      				humanObject.leftUpperArmy-=2.0f;
+      		}
+      		else if(mode == 6){
+      			// Left Upper Arm Rotation
+      			// cout<<(90+humanObject.leftUpperArmz)<<":"<<(180.0/M_PI)*acos(0.75*(sin(-1*humanObject.leftUpperArmz))) <<endl;
+      			if(humanObject.rightUpperArmz > 90.0f){
+      				if(humanObject.rightUpperArmy >= -90.0f){
+      					humanObject.rightUpperArmy -= 2.0f;
+      				}
+      			}
+      			else if(humanObject.rightLowerArmx > (180 - ((90-humanObject.rightUpperArmz) + (180.0/M_PI)*acos(0.75*(sin(humanObject.rightUpperArmz))) ))){
+      				if(humanObject.rightUpperArmy >= -30.0f)
+      					humanObject.rightUpperArmy -= 2.0f;
+      			}
+      			else
+      				if(humanObject.rightUpperArmy >= -90.0f){
+      					humanObject.rightUpperArmy -= 2.0f;
+      				}
+      		}
+      		else if(mode == 8){
+      			if(humanObject.hipy > -30.0f)
+      				humanObject.hipy -= 2.0f;
+      		}
+      		else if(mode == 9){
+      			if(humanObject.leftUpperLegy > -30.0f)
+      				humanObject.leftUpperLegy -= 2.0f;
+      		}
+      		else if(mode == 11){
+      			if(humanObject.rightUpperLegy > -30.0f)
+      				humanObject.rightUpperLegy -= 2.0f;
+      		}
+      	}
+      	glutPostRedisplay();
+	    break;	
+      }
+		/*	float headx;
+	float heady;
+	float headz;
 
+	float neckx;
+	float necky;
+	float neckz;
+
+	float torsox;
+	float torsoy;
+	float torsoz;
+
+	float leftUpperArmx;
+	float leftUpperArmy;
+	float leftUpperArmz;
+
+	float rightUpperArmx;
+	float rightUpperArmy;
+	float rightUpperArmz;
+
+	float leftLowerArmx;
+	float rightLowerArmx;
+
+	float hipx;
+	float hipy;
+	float hipz;
+	float hipTranslatex;
+	float hipTranslatey;
+	float hipTranslatez;
+
+	float leftUpperLegx;
+	float leftUpperLegy;
+	float leftUpperLegz;
+
+	float rightUpperLegx;
+	float rightUpperLegy;
+	float rightUpperLegz;
+
+	float leftLowerLegx;
+	float rightLowerLegx;
+*/      
       default:
       {
 
